a64713349071481635dcf406cc2f326052162d1f
marcusquigley <marcusquigley@gmail.com>
2014-01-12 10:52:41 -0500

    Added ViewModel project

---
+++ Northwind.ViewModel/ViewModelLocator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Northwind.ViewModel
+{
+
+    //this is a pattern impl that 
+    //1. exposes your ViewModel
+    //2. Instantiates that viewModel
+    //3. Can also be used for cleanup
+    public class ViewModelLocator
+    {
+        private static MainWindowViewModel _mainWindowViewModel;
+
+        public static MainWindowViewModel MainWindowViewModelStatic
+        {
+            get
+            {
+                return _mainWindowViewModel ?? (
+                 _mainWindowViewModel = new MainWindowViewModel()
+                 );
+            }
+        }
+    }
+}
