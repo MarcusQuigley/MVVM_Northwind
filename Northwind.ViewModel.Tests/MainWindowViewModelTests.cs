eaaf5ac666f016e11d4b6f87d5f5939c4da4a82c
marcusquigley <marcusquigley@gmail.com>
2014-01-12 17:04:11 -0500

    Added new Test project

---
+++ Northwind.ViewModel.Tests/MainWindowViewModelTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Northwind.ViewModel.Tests
+{
+    [TestClass]
+    public class MainWindowViewModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
