1459ec7d7de0ab0e74b57d0e5acb8fa10ae7e428
marcusquigley <marcusquigley@gmail.com>
2014-01-12 15:30:30 -0500

    Added changes due to the new .Data project

--- Northwind.ViewModel/MainWindowViewModel.cs
+++ Northwind.ViewModel/MainWindowViewModel.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Northwind.Data;
 
 namespace Northwind.ViewModel
 {
    public class MainWindowViewModel
     {
+       private IList<Customer> _customers;
+
+       public IList<Customer> Customers
+       {
+           get
+           {
+               return _customers ??
+               (_customers = new NorthwindEntities().Customers.ToList());
+           }
+       }
+       
     }
 }
