c2e24673548cea897c1aeabb49fc28f2d214aa5f
marcusquigley <marcusquigley@gmail.com>
2014-01-12 16:22:54 -0500

    Added new Data abstraction layer

---
+++ Northwind.UI.WPF/Northwind.Application/UIDataProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Northwind.Data;
+
+namespace Northwind.Application
+{
+    public interface IUIDataProvider
+    {
+        IList<Customer> GetCustomers();
+    }
+
+    public class UIDataProvider : IUIDataProvider
+    {
+
+        public IList<Customer> GetCustomers()
+        {
+            return new NorthwindEntities().Customers.ToList();
+        }
+    }
+}
