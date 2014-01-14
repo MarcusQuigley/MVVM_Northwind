20aa01c55a8d988539ac0bc09ed72e01bada06ff
marcusquigley <marcusquigley@gmail.com>
2014-01-14 10:32:08 -0500

    new tests

--- Northwind.ViewModel.Tests/MainWindowViewModelTests.cs
+++ Northwind.ViewModel.Tests/MainWindowViewModelTests.cs
@@ -6,6 +6,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rhino.Mocks;
 using Northwind.Application;
 using Northwind.Data;
+using System.Windows.Data;
 
 namespace Northwind.ViewModel.Tests
 {
@@ -24,6 +25,59 @@ namespace Northwind.ViewModel.Tests
 
             uiDataProviderMock.VerifyAllExpectations();
 
+        }
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void ShowCustomerDetails_SelectedCustomerIDIsNull_ThrowsException()
+        {
+            IUIDataProvider uiDataProviderMock = MockRepository.GenerateMock<IUIDataProvider>();
+
+            MainWindowViewModel target = new MainWindowViewModel(uiDataProviderMock);
+
+            target.ShowCustomerDetails();
+        }
+
+        [TestMethod]
+        public void ShowCustomerDetails_ToolNotFound_AddNewTool()
+        {
+            Customer expected = new Customer
+            {
+                CustomerID = "CustomerID"
+            };
+            MainWindowViewModel target = GetShowCustomerDetailsTarget(expected);
+            target.ShowCustomerDetails();
+            CustomerDetailsViewModel actual =
+              target.Tools.OfType<CustomerDetailsViewModel>().FirstOrDefault(viewmodel =>
+                  viewmodel.Customer.CustomerID == expected.CustomerID);
+
+            Assert.IsNotNull(actual);
+            
+        }
+
+        [TestMethod]
+        public void ShowCustomerDetails_Always_ToolIsSetToCurrent()
+        {
+            Customer expected = new Customer
+            {
+                CustomerID = "CustomerID"
+            };
+            MainWindowViewModel target = GetShowCustomerDetailsTarget(expected);
+            target.ShowCustomerDetails();
+            CustomerDetailsViewModel actual =
+            (CustomerDetailsViewModel)CollectionViewSource.GetDefaultView(target.Tools).CurrentItem;
+
+            Assert.AreSame(expected, actual.Customer);
+        }
+
+        private MainWindowViewModel GetShowCustomerDetailsTarget(Customer customer)
+        { 
+            IUIDataProvider uiDataProviderStub = MockRepository.GenerateStub<IUIDataProvider>();
+
+            MainWindowViewModel target = new MainWindowViewModel(uiDataProviderStub);
+            target.SelectedCustomerID = customer.CustomerID;
+            uiDataProviderStub.Stub(d => d.GetCustomer(customer.CustomerID)).Return(customer);
+
+            return target;
         }
     }
 }
