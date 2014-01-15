b06d7f8825717412948d87e79a5748ef1443de78
marcusquigley <marcusquigley@gmail.com>
2014-01-15 12:06:59 -0500

    Adding NW.Model Project - Custom data model

---
+++ Northwind.Model/ModelBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Northwind.Model
+{
+    public class ModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            VerifyPropertyName(propertyName);
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        [DebuggerStepThrough]
+        [Conditional("DEBUG")]
+        private void VerifyPropertyName(string propertyName)
+        {
+            if (TypeDescriptor.GetProperties(this.GetType().GetProperty(propertyName)) == null)
+                throw new InvalidOperationException(
+                    string.Format("{0} is not a valid property", propertyName));
+        }
+    }
+}
